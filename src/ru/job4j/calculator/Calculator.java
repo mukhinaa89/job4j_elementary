4c2ca71e8543ea28574ffdcda1dbe7b5a57048ee
Mukhin <mukhinaa89@yandex.ru>
2020-12-13 10:30:58 +0300

    6. Идеальный вес [#427531] исправлена формула в методе womanWeight

--- src/ru/job4j/calculator/Calculator.java
+++ src/ru/job4j/calculator/Calculator.java
@@ -2,18 +2,11 @@ package ru.job4j.calculator;
 
 public class Calculator {
     public static void main(String[] args) {
-        int one = 1;
-        int two = 2;
-        int onePlusTwo = one + two;
-        int six = 6;
-        int four = 4;
-        int five = 5;
-        int sixDivTwo = six / two;
-        int fiveMinusTwo = five - two;
-        int fourTimeTwo = four * two;
-        System.out.println(onePlusTwo);
-        System.out.println(sixDivTwo);
-        System.out.println(fiveMinusTwo);
-        System.out.println(fourTimeTwo);
+        int num = 11;
+        int div = num / 3;
+        int mod = num % 4;
+        System.out.println(div);
+        System.out.println(mod);
     }
+
 }
