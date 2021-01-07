efbe8cf39010da2eb913aff6ea1e732511cfea67
Mukhin <mukhinaa89@yandex.ru>
2021-01-07 18:12:21 +0300

    6.2.3. Перевернуть массив. [#427515]

---
+++ test/ru/job4j/array/TurnTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class TurnTest {
+    @Test
+    public void whenTurnArrayWithEvenAmountOfElementsThenTurnedArray() {
+        int[] input = new int[] {4, 1, 6, 2};
+        int[] result = Turn.back(input);
+        int[] expect = new int[] {2, 6, 1, 4};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenTurnArrayWithOddAmountOfElementsThenTurnedArray() {
+        int[] input = new int[] {4, 1, 6, 0, 2};
+        int[] result = Turn.back(input);
+        int[] expect = new int[] {2, 0, 6, 1, 4};
+        assertThat(result, is(expect));
+        //напишите здесь тест, проверяющий переворот массива с нечётным числом элементов, например {1, 2, 3, 4, 5}.
+    }
+}
