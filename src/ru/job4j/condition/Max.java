e83d780e06b7d55670d63d48fb5d18dad89cd4c2
Mukhin <mukhinaa89@yandex.ru>
2021-03-01 10:26:05 +0300

    3. Перегрузить метод max для трех чисел [#427690]

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -1,12 +1,15 @@
 package ru.job4j.condition;
 
 public class Max {
-    public static int max(int left, int right) {
-        return  left > right ? left : right;
+    public static int max(int first, int second) {
+        return  first > second ? first : second;
     }
 
-    public static void main(String[] args) {
-        int result = Max.max(44, 34);
-        System.out.println("Максимальное значение " + result);
+    public static int max(int first, int second, int third) {
+        return max(max(first, second), third);
+    }
+
+    public static int max(int first, int second, int third, int four) {
+        return max(max(first, second), max(third, four));
     }
 }
--- test/ru/job4j/condition/MaxTest.java
+++ test/ru/job4j/condition/MaxTest.java
@@ -18,8 +18,62 @@ public class MaxTest {
     }
 
     @Test
-    public void whenLeft2Right2Then2() {
+    public void whenFirst2Second2Then2() {
         int result = Max.max(2, 2);
         assertThat(result, is(2));
+    }
+
+    @Test
+    public void whenFirst3Second2Third1Then3() {
+        int result = Max.max(3, 2, 1);
+        assertThat(result, is(3));
+    }
+
+    @Test
+    public void whenFirst3Second5Third3Then5() {
+        int result = Max.max(3, 5, 3);
+        assertThat(result, is(5));
+    }
+
+    @Test
+    public void whenFirst4Second5Third6Then6() {
+        int result = Max.max(4, 5, 6);
+        assertThat(result, is(6));
+    }
+
+    @Test
+    public void whenFirst3Second3Third3Then3() {
+        int result = Max.max(3, 3, 3);
+        assertThat(result, is(3));
+    }
+
+    @Test
+    public void whenFirst1Second2Third3Four4Then3() {
+        int result = Max.max(1, 2, 3, 4);
+        assertThat(result, is(4));
+    }
+
+    @Test
+    public void whenFirst1Second2Third5Four3Then5() {
+        int result = Max.max(1, 2, 5, 3);
+        assertThat(result, is(5));
+    }
+
+    @Test
+    public void whenFirst1Second6Third2Four3Then6() {
+        int result = Max.max(1, 6, 2, 3);
+        assertThat(result, is(6));
+    }
+
+    @Test
+    public void whenFirst7Second1Third2Four3Then6() {
+        int result = Max.max(7, 1, 2, 3);
+        assertThat(result, is(7));
+    }
+
+    @Test
+    public void whenFirst8Second8Third8Four8Then8() {
+        int result = Max.max(8, 8, 8, 8);
+        assertThat(result, is(8));
     }
 }
